e45760c661d2f68dced5a29d2d7b0471f0973bf5
liuxu <xu.liu08@ele.me>
2020-01-13 15:05:11 +0800

    simple ForkJoinPool example

---
+++ concurrency/corejava/forkjoin/ForkJoinTest.java
@@ -0,0 +1,20 @@
+package concurrency.corejava.forkjoin;
+
+import java.util.concurrent.ForkJoinPool;
+
+public class ForkJoinTest {
+    public static void main(String[] args) {
+        final int SIZE = 10000000;
+
+        var numbers = new double[SIZE];
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = Math.random();
+        }
+
+        var counter = new Counter(numbers, 0, numbers.length, x -> x > 0.5);
+        var pool = new ForkJoinPool();
+        pool.invoke(counter);
+
+        System.out.println(counter.join());
+    }
+}
