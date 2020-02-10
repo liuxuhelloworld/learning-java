89f728822422c4da26bcd5988927b9c0586a79d3
liuxu <xu.liu08@ele.me>
2020-02-10 11:08:03 +0800

    Revert "generic varargs example"
    This reverts commit b370796cd45a7d8282557f7560584bb561f1e248.

--- generics/corejava/GenericVariableArguments.java
+++ generics/corejava/GenericVariableArguments.java
@@ -3,22 +3,26 @@ package generics.corejava;
 import java.util.ArrayList;
 import java.util.Collection;
 
-public class GenericVarargsTest {
+import generics.corejava.Pair;
+
+public class GenericVariableArguments {
     public static <T> void addAll(Collection<T> coll, T... args) {
         for (T t : args) {
             coll.add(t);
         }
     }
 
-    public static <E> E[] array(E... args) {
-        return args;
+    public static <T> T[] array(T... args) {
+       return args;
     }
 
     public static void main(String[] args) {
         Collection<Pair<String>> coll = new ArrayList<>();
         Pair<String> pair1 = new Pair<>("abc", "def");
         Pair<String> pair2 = new Pair<>("ghi", "jkl");
+
         addAll(coll, pair1, pair2);
+
         for (Pair<String> pair : coll) {
             System.out.println(pair);
         }
