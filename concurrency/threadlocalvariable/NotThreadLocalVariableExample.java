743582acc500a8635f7a1dd99d1c584e141fdf3d
liuxu <liuxuhelloworld@126.com>
2020-04-09 20:38:19 +0800

    thread local variable example

---
+++ concurrency/threadlocalvariable/NotThreadLocalVariableExample.java
@@ -0,0 +1,29 @@
+package concurrency.threadlocalvariable;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class NotThreadLocalVariableExample {
+    private static SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+    public static void main(String[] args) {
+        String date = "2020-04-09 12:40:03";
+
+        ExecutorService executorService = Executors.newCachedThreadPool();
+
+        for (int i = 0; i < 10; i++) {
+            executorService.execute(() -> {
+                for (int j = 0; j < 5; j++) {
+                    try {
+                        System.out.println(dateFormat.parse(date));
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+        executorService.shutdown();
+    }
+}
